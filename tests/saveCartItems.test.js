b7d815c2cff7728ae8b58869c3d184827c381706
thiago maia <thiago.maia@live.com>
2022-04-02 18:30:13 -0300

    10 terminado

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,7 +8,8 @@ describe('4 - Teste a função saveCartItems', () => {
     await saveCartItems('<ol><li>Item</li></ol>')
     expect(localStorage.setItem).toHaveBeenCalled()
   })
-  it('ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems')
-    saveCartItems('<ol><li>Item</li></ol>')
-    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', "<ol><li>Item</li></ol>")
+  it('saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, primeiro cartItems e o segundo argumento para saveCartItems', async () =>{
+    await saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', JSON.stringify('<ol><li>Item</li></ol>'))
+  })
 });
